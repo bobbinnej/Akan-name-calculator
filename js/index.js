d8b31ff9e48fd4b1e2e63267b7c0903afa1cd0c1
jackie <bobbinnej@gmail.com>
2022-03-06 16:20:06 +0300

    Date validation

--- js/index.js
+++ js/index.js
@@ -1,19 +1,38 @@
 /*Call the onsubmit() function from html file */
 
-function getInput(){
-    let dd =parseInt(prompt("Enter day of birth"));
-    let mm=parseInt(prompt("Enter month of birth"));
-    let yy=parseInt(prompt("Enter year of birth"));
 
-    console.log("User entered birth day:",dd);
-    console.log("User entered birth month:", mm);
-    console.log("User entered birth year:",yy);
-    getOutput(dd,mm,yy);
+/*Gather user input from html to js */
+function myInput(){
+    
+ var days=document.getElementById("days").value;
+ var months=document.getElementById("months").value;
+ var years=document.getElementById("years").value;
+
+ var flexRadioDefault =document.getElementsByName("flexRadioDefault");
+
+ var i;
+ for(i=0; i<flexRadioDefault.length; i++){
+     if(flexRadioDefault[i].checked){
+         alert(flexRadioDefault[i].value);
+     }
+   
+     
+
+ }
+
+ if(days <=0){
+
+    console.log("invalid date");
+    alert("invalid date");
+ }
+ else if(days>31){
+     console.log("Invalid");
+     alert("invalid date");
+ }
+ else{
+     alert("date inputted");
+ }
 
 }
-getInput()
-function getOutput(dd,mm,yy){
-    console.log("Akan name is",dd*mm*yy);
-    return("Akan name is ",dd*mm*yy);
-}
+
 
