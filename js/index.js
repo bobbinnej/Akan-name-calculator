786203dc44ff9bfe5c25c90d2aebb77aecd001d3
jackie <bobbinnej@gmail.com>
2022-03-06 13:10:13 +0300

    Edit onsubmit even to onclick

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-/* */
+/*Call the onsubmit() function from html file */
+
+function getInput(){
+    let dd =parseInt(prompt("Enter day of birth"));
+    let mm=parseInt(prompt("Enter month of birth"));
+    let yy=parseInt(prompt("Enter year of birth"));
+
+    console.log("User entered birth day:",dd);
+    console.log("User entered birth month:", mm);
+    console.log("User entered birth year:",yy);
+    getOutput(dd,mm,yy);
+
+}
+getInput()
+function getOutput(dd,mm,yy){
+    console.log("Akan name is",dd*mm*yy);
+    return("Akan name is ",dd*mm*yy);
+}
+
