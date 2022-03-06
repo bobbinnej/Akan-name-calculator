56d11c939742983978ba4ef425be8a24761d7981
jackie <bobbinnej@gmail.com>
2022-03-06 19:05:21 +0300

    Get the century from year

--- js/index.js
+++ js/index.js
@@ -1,38 +1,77 @@
-/*Call the onsubmit() function from html file */
-
 
 /*Gather user input from html to js */
 function myInput(){
     
- var days=document.getElementById("days").value;
- var months=document.getElementById("months").value;
- var years=document.getElementById("years").value;
-
- var flexRadioDefault =document.getElementsByName("flexRadioDefault");
-
- var i;
- for(i=0; i<flexRadioDefault.length; i++){
-     if(flexRadioDefault[i].checked){
-         alert(flexRadioDefault[i].value);
-     }
+    var days=document.getElementById("days").value;
+    var months=document.getElementById("months").value;
+    var years=document.getElementById("years").value;
    
-     
+    var flexRadioDefault =document.getElementsByName("flexRadioDefault");
+   
+    var i;
+    for(i=0; i<flexRadioDefault.length; i++){
+        if(flexRadioDefault[i].checked){
+            alert(flexRadioDefault[i].value);
+        }
+      
+        
+   
+    }
+   
+   
+   
+   
+   
+   
+   /** Date validation*/
+    if(days <=0){
+   
+       console.log("invalid date");
+       alert("invalid date");
+    }
+    else if(days>31){
+        console.log("Invalid date");
+        alert("invalid date");
+    }
+    else{
+        console.log("Date of birth inputted as");
+       return days;
+    }
+   
 
- }
 
- if(days <=0){
 
-    console.log("invalid date");
-    alert("invalid date");
- }
- else if(days>31){
-     console.log("Invalid");
-     alert("invalid date");
+   /** month validation*/
+   if(months >12){
+   
+    console.log("invalid month");
+    alert("invalid month");
  }
  else{
-     alert("date inputted");
+     return months;
  }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
